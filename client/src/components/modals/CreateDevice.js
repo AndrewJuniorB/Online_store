13e0d6281be1d519d2841049417b6007ef35082c
AndrewBurlakov <andrey.i.burlakov@gmail.com>
2021-12-11 11:31:16 -0800

    Finish Modal form for add device new description

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
-import { Button, Form, Dropdown } from 'react-bootstrap';
+import { Button, Form, Dropdown, Col, Row} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import { Context } from '../../index.js';
 import { observer } from 'mobx-react-lite';
 
 const CreateDevice = observer(({ show, onHide }) => {
   const { device } = useContext(Context);
+  const [info, setInfo] = useState([]);
+
+  const addInfo = () => {
+    setInfo([...info, {title: '', description: '', number: Date.now()}])
+  };
+
+  const removeInfo = (number) => {
+    setInfo(info.filter(i => i.number !== number))
+  };
+
   return (
     <Modal
       show={show}
@@ -58,8 +69,35 @@ const CreateDevice = observer(({ show, onHide }) => {
             className='mt-3'
             type='file'
           />
+
+          <hr/>
+          <Button
+            variant={'outline-info'}
+            onClick={addInfo}
+          >
+            Add product information
+          </Button>
+          {info.map (i =>
+
+            <Row className='mt-4' key={i.number}>
+              <Col md={4}>
+                <Form.Control placeholder='Type of description'/>
+              </Col>
+              <Col md={4}>
+                <Form.Control placeholder='Description'/>
+              </Col>
+              <Col md={4}>
+              <Button
+              onClick={() => removeInfo(i.number)}
+              variant={'outline-danger'}
+              >
+                Delete?
+              </Button>
+              </Col>
+            </Row>
+
+          )}
         </Form>
-        <hr/>
       </Modal.Body>
       <Modal.Footer>
         <Button variant='outline-danger' onClick={onHide}>Close</Button>
